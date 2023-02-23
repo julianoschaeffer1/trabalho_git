8ce54e1dee407bd1124e0f7d5cf7359d4bd6114e
Juliano Lucena Schaeffer <julianoschaeffer.ss014@academico.ifsul.edu.br>
2023-02-23 16:50:22 -0300

    Adicionado funcionalidade digitar o nome e imprimir na tela

--- TRABALHO_GIT_2I/src/main.java
+++ TRABALHO_GIT_2I/src/main.java
@@ -1,14 +1,13 @@
-
+import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
-System.out.println("Ola Mundo");
+		Scanner t = new Scanner(System.in);
+		System.out.println("Digite o seu nome:");
+		String a = t.nextLine();
+		System.out.println("Olá"+a);
+		System.out.println("Ola Mundo");
 
-		
-		
-		
-		
-		
 	}
 
 }
